948c342bd0469eb1af8a0a5205b8ee68a072d2af
Aroslav Kochkin <kochkin.2328@inbox.ru>
2025-07-07 22:46:18 +0700

    first commit

---
+++ ShiftTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShiftTestApp
+//
+//  Created by Ярослав Кочкин on 07.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .cyan
+	}
+
+
+}
+
